07feb215bf71a4a9fcc3378192b672b191cebc7d
Emily Port <emilylport@gmail.com>
2022-10-31 11:29:47 -0600

    delete commented out code

--- app/models/supermarket.rb
+++ app/models/supermarket.rb
@@ -2,20 +2,6 @@ class Supermarket < ApplicationRecord
   has_many :items
 
   def customer_list 
-    # items.map {|item| item.customers.map{|customer| customer.name}}.flatten.uniq
     items.map {|item| item.customers.distinct.pluck(:name)}.flatten.uniq 
-    # items.each do |item|
-      #item.customers.pluck(:name)
-    # end
-    # @items = Item.all 
-    # @item = Item.find(params[:id])
-    # @item = Customer.find(params[:id])
-
-    # CustomerItem.create!(customer: @customer, item: @item)
-    # @items.each do |item|
-    #   item.customers
-      
-    # end
-    # Customer.find(:id)
   end
 end
