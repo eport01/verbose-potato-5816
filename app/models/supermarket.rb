fb118b5c5495ebfdfb4c3817c0cd09a1905b3bc7
Emily Port <emilylport@gmail.com>
2022-10-31 11:34:05 -0600

    add a has many customers through items association in supermarket model

--- app/models/supermarket.rb
+++ app/models/supermarket.rb
@@ -1,7 +1,13 @@
 class Supermarket < ApplicationRecord
   has_many :items
+  has_many :customers, through: :items
+
+  # def customer_list 
+  #   items.map {|item| item.customers.distinct.pluck(:name)}.flatten.uniq 
+  # end
+
 
   def customer_list 
-    items.map {|item| item.customers.distinct.pluck(:name)}.flatten.uniq 
+    customers.select(:name).distinct.pluck(:name) 
   end
 end
